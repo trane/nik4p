fb8b1f80f98c99ded0651771ecb10422d5298806
Andrew Kuhnhausen <akuhnhausen@periscopedata.com>
2018-11-08 16:01:00 -0800

    Fix up database code

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from psycopg2.extensions import parse_dsn
 
 dsn = os.environ['DATABASE_URI']
 connect_dict = parse_dsn(dsn)
-con = psycopg2.connect(**connect_dic)
+con = psycopg2.connect(connect_timeout=1, **connect_dict)
 cur = con.cursor()
-cur.execute("select * from users limit 1")
+cur.execute("select firstname,lastname from users limit 1")
 cur.fetchone()
